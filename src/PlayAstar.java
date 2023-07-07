fced7fe9514ce704d5cdbd1a1896f3f08b1f0a7f
lltlien <21522282@gm.uit.edu.vn>
2023-07-07 21:23:51 +0700

    update Astar code

--- src/PlayAstar.java
+++ src/PlayAstar.java
@@ -12,6 +12,12 @@ import engine.core.MarioGame;
 import engine.core.MarioResult;
 
 public class PlayAstar {
+    public static String baseDir = "./levels/evaluation";
+    public static String levelPaths = baseDir + "/level/";
+    public static String AStarDir = baseDir + "/astar/";
+    public static String resDir = baseDir + "/astar_result/";
+    public static String resPath = "";
+
     public static void printResults(MarioResult result) {
         System.out.println("****************************************************************");
         System.out.println("Game Status: " + result.getGameStatus().toString() +
@@ -19,12 +25,27 @@ public class PlayAstar {
         System.out.println("Lives: " + result.getCurrentLives() + " Coins: " + result.getCurrentCoins() +
                 " Remaining Time: " + (int) Math.ceil(result.getRemainingTime() / 1000f));
         System.out.println("Mario State: " + result.getMarioMode() +
-                " (Mushrooms: " + result.getNumCollectedMushrooms() + " Fire Flowers: " + result.getNumCollectedFireflower() + ")");
+                " (Mushrooms: " + result.getNumCollectedMushrooms() + " Fire Flowers: "
+                + result.getNumCollectedFireflower() + ")");
         System.out.println("Total Kills: " + result.getKillsTotal() + " (Stomps: " + result.getKillsByStomp() +
                 " Fireballs: " + result.getKillsByFire() + " Shells: " + result.getKillsByShell() +
                 " Falls: " + result.getKillsByFall() + ")");
         System.out.println("Bricks: " + result.getNumDestroyedBricks() + " Jumps: " + result.getNumJumps() +
                 " Max X Jump: " + result.getMaxXJump() + " Max Air Time: " + result.getMaxJumpAirTime());
+        
+        if (resPath != "") {
+            System.out.println(resPath);
+            try {
+                var fres = new FileWriter(resPath);
+                fres.write(result.getGameStatus().toString());
+                fres.close();
+            } catch (Exception e) {
+                System.out.println("Error on write result file!");
+                System.out.println(e.getMessage());
+            }
+            
+        }
+
         System.out.println("****************************************************************");
     }
 
@@ -40,38 +61,40 @@ public class PlayAstar {
     public static String playAstar(MarioGame game, String levelPath, boolean render) {
         System.out.println("Running Astar agent with render: " + render);
         System.out.println("=========================");
-        MarioResult result = game.playAstar(new agents.robinBaumgarten.Agent(), getLevel(levelPath), 200, render);
+        MarioResult result = game.playAstar(new agents.robinBaumgarten.Agent(), getLevel(levelPath), 20, render);
         printResults(result);
         return result.getAStarResult();
     }
+
     public static String playAstar(MarioGame game, String levelPath, boolean render, String imgDir) {
         System.out.println("Running Astar agent with render: " + render);
         System.out.println("=========================");
-        MarioResult result = game.playAstar(new agents.robinBaumgarten.Agent(), getLevel(levelPath), 200, render, imgDir);
+        MarioResult result = game.playAstar(new agents.robinBaumgarten.Agent(), getLevel(levelPath), 20, render,
+                imgDir);
         printResults(result);
         return result.getAStarResult();
     }
 
     public Set<String> listFilesUsingFilesList(String dir) throws IOException {
-    try (Stream<Path> stream = Files.list(Paths.get(dir))) {
-        return stream
-          .filter(file -> !Files.isDirectory(file))
-          .map(Path::getFileName)
-          .map(Path::toString)
-          .collect(Collectors.toSet());
+        try (Stream<Path> stream = Files.list(Paths.get(dir))) {
+            return stream
+                    .filter(file -> !Files.isDirectory(file))
+                    .map(Path::getFileName)
+                    .map(Path::toString)
+                    .collect(Collectors.toSet());
+        }
     }
-}
 
     public static void main(String[] args) {
-        String levelPaths = "./levels/evaluation/level/";
-        String AStarDir = "./levels/evaluation/astar/";
 
         File tmpLvlPath = new File(levelPaths);
         String levels[] = tmpLvlPath.list();
-        for(int i = 0; i < levels.length; i++) {
+        for (int i = 0; i < levels.length; i++) {
+            System.out.printf("[LEVEL] %d/100\n", i+1);
             MarioGame game = new MarioGame();
             String levelPath = levelPaths + levels[i];
-            String res = playAstar(game, levelPath, true);
+            resPath = resDir + levels[i];
+            String res = playAstar(game, levelPath, false);
             try {
                 File lvl = new File(levelPath);
                 var fAStar = new FileWriter(AStarDir + lvl.getName());
@@ -81,74 +104,7 @@ public class PlayAstar {
                 System.out.println("An error occurred.");
                 e.printStackTrace();
             }
+            resPath = "";
         }
     }
 }
-
-
-// import java.io.IOException;
-// import java.nio.file.Files;
-// import java.nio.file.Paths;
-
-// import engine.core.MarioGame;
-// import engine.core.MarioResult;
-
-// public class PlayAstar {
-//     public static void printResults(MarioResult result) {
-//         System.out.println("****************************************************************");
-//         System.out.println("Game Status: " + result.getGameStatus().toString() +
-//                 " Percentage Completion: " + result.getCompletionPercentage());
-//         System.out.println("Lives: " + result.getCurrentLives() + " Coins: " + result.getCurrentCoins() +
-//                 " Remaining Time: " + (int) Math.ceil(result.getRemainingTime() / 1000f));
-//         System.out.println("Mario State: " + result.getMarioMode() +
-//                 " (Mushrooms: " + result.getNumCollectedMushrooms() + " Fire Flowers: " + result.getNumCollectedFireflower() + ")");
-//         System.out.println("Total Kills: " + result.getKillsTotal() + " (Stomps: " + result.getKillsByStomp() +
-//                 " Fireballs: " + result.getKillsByFire() + " Shells: " + result.getKillsByShell() +
-//                 " Falls: " + result.getKillsByFall() + ")");
-//         System.out.println("Bricks: " + result.getNumDestroyedBricks() + " Jumps: " + result.getNumJumps() +
-//                 " Max X Jump: " + result.getMaxXJump() + " Max Air Time: " + result.getMaxJumpAirTime());
-//         System.out.println("****************************************************************");
-//     }
-
-//     public static String getLevel(String filepath) {
-//         String content = "";
-//         try {
-//             content = new String(Files.readAllBytes(Paths.get(filepath)));
-//         } catch (IOException e) {
-//         }
-//         return content;
-//     }
-
-//     public static void playAstar(MarioGame game, String level, boolean render) {
-//         System.out.println("Running Astar agent with render: " + render);
-//         System.out.println("=========================");
-//         printResults(game.playAstar(new agents.robinBaumgarten.Agent(), level, 200, render));
-//     }
-//     public static void playAstar(MarioGame game, String level, boolean render, String imgDir) {
-//         System.out.println("Running Astar agent with render: " + render);
-//         System.out.println("=========================");
-//         printResults(game.playAstar(new agents.robinBaumgarten.Agent(), level, 200, render, imgDir));
-//     }
-
-//     public static void main(String[] args) {
-//         MarioGame game = new MarioGame();
-//         boolean render = true;
-//         String level = "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\r\n" + //
-//                 "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\r\n" + //
-//                 "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\r\n" + //
-//                 "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\r\n" + //
-//                 "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\r\n" + //
-//                 "----------------------------------------------------------------------------------g-----------------------------------------------------------------------------------------------------------------------\r\n" + //
-//                 "----------------------!---------------------------------------------------------SSSSSSSS---SSS!--------------@-----------SSS----S!!S--------------------------------------------------------##------------\r\n" + //
-//                 "-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------###------------\r\n" + //
-//                 "-------------------------------------------------------------------------------g----------------------------------------------------------------------------------------------------------####------------\r\n" + //
-//                 "----------------------------------------------------------------1------------------------------------------------------------------------------------------------------------------------#####------------\r\n" + //
-//                 "----------------!---S@S!S---------------------tt---------tt------------------S@S--------------C-----SU----!--!--!-----S----------SS------#--#----------##--#------------SS!S------------######------------\r\n" + //
-//                 "--------------------------------------tt------tt---------tt-----------------------------------------------------------------------------##--##--------###--##--------------------------#######------------\r\n" + //
-//                 "----------------------------tt--------tt------tt---------tt----------------------------------------------------------------------------###--###------####--###-----tt--------------tt-########--------F---\r\n" + //
-//                 "---M-----------------g------tt--------tt-g----tt-----g-g-tt------------------------------------g-g--------k-----------------gg-g-g----####--####----#####--####----tt---------gg---tt#########--------#---\r\n" + //
-//                 "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX--XXXXXXXXXXXXXXX---XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX--XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\r\n" + //
-//                 "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX--XXXXXXXXXXXXXXX---XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX--XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-//         playAstar(game, level, render);
-//     }
-// }
