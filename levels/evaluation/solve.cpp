dbf84220b4ad5ce91eb7f2bce2d1787230031bd1
lltlien <21522282@gm.uit.edu.vn>
2023-07-07 21:23:18 +0700

    add data

---
+++ levels/evaluation/solve.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <sys/types.h>
+
+using namespace std;
+const string BASE_DIR = "./";
+const string LEVEL_DIR = BASE_DIR + "level/";
+const string ASTAR_DIR = BASE_DIR + "astar/";
+const string ASTAR_RES_DIR = BASE_DIR + "astar_result/";
+const string PROMPTS_DIR = BASE_DIR + "prompts/";
+
+
+int main(int argc, char** argv) { 
+}
+
