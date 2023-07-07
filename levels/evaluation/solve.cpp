7e3a68e6b6a2b9329db5e57e4c2633ea179d7c52
lltlien <21522282@gm.uit.edu.vn>
2023-07-07 23:12:13 +0700

    solve prompt percent

--- levels/evaluation/solve.cpp
+++ levels/evaluation/solve.cpp
@@ -10,7 +10,17 @@ const string ASTAR_DIR = BASE_DIR + "astar/";
 const string ASTAR_RES_DIR = BASE_DIR + "astar_result/";
 const string PROMPTS_DIR = BASE_DIR + "prompts/";
 
+int L = 0, R = 80;
+void solvePrompt(string dir) {
+    for (int i = L; i <= R; i++) {
+        string filename = to_string(i) + ".txt";
+        string filepath = dir + filename;
+        cout << filepath << endl;
+        
+    }
+}
 
 int main(int argc, char** argv) { 
+    solvePrompt(PROMPTS_DIR);
 }
 
